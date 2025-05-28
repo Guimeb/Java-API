8406e0fa6d6a34c2d9ffcf0bd536f78b7090bc21
Guimeb <rm551981@fiap.com.br>
2025-05-28 04:47:24 -0300

    Inicializando db local

--- src/main/java/br/com/sprint/sprint/controller/UserController.java
+++ src/main/java/br/com/sprint/sprint/controller/UserController.java
@@ -2,8 +2,17 @@ package br.com.sprint.sprint.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/id")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
 }
--- src/main/java/br/com/sprint/sprint/service/UserService.java
+++ src/main/java/br/com/sprint/sprint/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.sprint.sprint.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.sprint.sprint.dto.UserRequestCreate;
 import br.com.sprint.sprint.repository.UserRepository;
 import br.com.sprint.sprint.model.User;
 
